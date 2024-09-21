4e3f66b3df89d7a9383a8c8ced23c4e708f1bfbb
Ben <bin.fang@edu.sait.ca>
2024-09-21 15:40:25 -0600

    adds week 2 assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Bin Fang</p>
+            <Link href="https://github.com/binfang1/cprg306-assignments">github</Link>
+        </div>
+    );
+}
